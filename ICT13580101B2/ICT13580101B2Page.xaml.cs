ca346d3ce25d0c126b27ae300c82b8bfc503cd5b
Archawaphol101 <poohbloody@gmail.com>
2017-12-19 19:01:27 +0700

    ICT13580101B2
    complete

---
+++ ICT13580101B2/ICT13580101B2Page.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace ICT13580101B2
+{
+    public partial class ICT13580101B2Page : ContentPage
+    {
+        public ICT13580101B2Page()
+        {
+            InitializeComponent();
+        }
+    }
+}
